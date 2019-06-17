108a6c15521198d4a7ee68c84ebd7e4e91963053
Dare Lawal <lawaldare@gmail.com>
2019-06-17 15:53:28 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+const modelParams = {
+  flipHorizontal: true, // flip e.g for video
+  imageScaleFactor: 0.7, // reduce input image size for gains in speed.
+  maxNumBoxes: 20, // maximum number of boxes to detect
+  iouThreshold: 0.5, // ioU threshold for non-max suppression
+  scoreThreshold: 0.79 // confidence threshold for predictions.
+};
+
+navigator.getUserMedia =
+  navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia ||
+  navigator.msGetUserMedia;
+
+const video = document.getElementById('video');
+const audio = document.getElementById('audio');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+let model;
+
+handTrack.startVideo(video).then(status => {
+  if (status) {
+    navigator.getUserMedia(
+      { video: {} },
+      stream => {
+        video.srcObject = stream;
+        setInterval(runDetection, 1000);
+      },
+      err => console.log(err)
+    );
+  }
+});
+
+const runDetection = () => {
+  model.detect(video).then(predictions => {
+    //model.renderPredictions(predictions, canvas, context, video);
+    if (predictions.length > 0) {
+      audio.play();
+    }
+  });
+};
+
+handTrack.load(modelParams).then(lmodel => {
+  model = lmodel;
+});
